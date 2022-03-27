e985381d9176e223663771b2c7ab778b10f64714
Will <wngalles@iastate.edu>
2022-03-27 12:37:16 -0500

    Had one line wrong everything works now

--- src/TopLevel/ALU/ALU.vhd
+++ src/TopLevel/ALU/ALU.vhd
@@ -180,7 +180,7 @@ begin
 
 
   s_OpSelect(6) <=  i_In2(15 downto 0) & x"0000"; --Lui
-  s_OpSelect(7) <=  s_OpSelect(2); --Unsigned       
+  s_OpSelect(7) <=  s_OpSelect(0); --Unsigned       
 
   MUX_Op: mux32t1_8 
   port map(
