5c327387d59eb9406891b13be8a9c6c5ab2b45f4
Will <wngalles@iastate.edu>
2022-04-13 13:11:07 -0500

    Software Fib

--- mips/fibonacci.s
+++ mips/fibonacci.s
@@ -4,8 +4,10 @@ fibs:.word   0 : 19         # "array" of words to contain fib values
 size: .word  19             # size of "array" (agrees with array declaration)
 prompt: .asciiz "How many Fibonacci numbers to generate? (2 <= x <= 19)"
 .text
-      la   $s0, fibs        # load address of array
-      la   $s5, size        # load address of size variable
+      lasw   $s0, fibs        # load address of array
+      lasw   $s5, size        # load address of size variable
+      nop
+      nop
       lw   $s5, 0($s5)      # load array size
 
 # Optional: user inputs the number of Fibonacci numbers to generate
@@ -19,6 +21,8 @@ prompt: .asciiz "How many Fibonacci numbers to generate? (2 <= x <= 19)"
 #      add  $s5, $v0, $zero  # transfer the number to the desired register
       
       li   $s2, 1           # 1 is the known value of first and second Fib. number
+      nop
+      nop
       sw   $s2, 0($s0)      # F[0] = 1
       sw   $s2, 4($s0)      # F[1] = F[0] = 1
       addi $s1, $s5, -2     # Counter for loop, will execute (size-2) times
@@ -26,47 +30,61 @@ prompt: .asciiz "How many Fibonacci numbers to generate? (2 <= x <= 19)"
       # Loop to compute each Fibonacci number using the previous two Fib. numbers.
 loop: lw   $s3, 0($s0)      # Get value from array F[n-2]
       lw   $s4, 4($s0)      # Get value from array F[n-1]
+      nop
+      nop
       add  $s2, $s3, $s4    # F[n] = F[n-1] + F[n-2]
+      nop
+      nop
       sw   $s2, 8($s0)      # Store newly computed F[n] in array
       addi $s0, $s0, 4      # increment address to now-known Fib. number storage
       addi $s1, $s1, -1     # decrement loop counter
+      nop
+      nop
       bne $s1, $zero, loop  # repeat while not finished
-      
+      nop
+
       # Fibonacci numbers are computed and stored in array. Print them.
-      la   $a0, fibs        # first argument for print (array)
+      lasw   $a0, fibs        # first argument for print (array)
       add  $a1, $zero, $s5  # second argument for print (size)
       jal  print            # call print routine. 
+      nop
 
       # The program is finished. Exit.
       halt
       j die 
-		
+      nop 
 ###############################################################
 # Subroutine to print the numbers on one line.
       .data
 space:.asciiz  " "          # space to insert between numbers
 head: .asciiz  "The Fibonacci numbers are:\n"
       .text
-print:add  $t0, $zero, $a0  # starting address of array of data to be printed
+print:nop
+      add  $t0, $zero, $a0  # starting address of array of data to be printed
       add  $t1, $zero, $a1  # initialize loop counter to array size
-      la   $a0, head        # load address of the print heading string
+      lasw   $a0, head        # load address of the print heading string
       ori  $v0, $zero , 4           # specify Print String service
       syscall               # print the heading string
       
-out:  lw   $a0, 0($t0)      # load the integer to be printed (the current Fib. number)
+out:  nop
+      lw   $a0, 0($t0)      # load the integer to be printed (the current Fib. number)
       ori  $v0, $zero , 1           # specify Print Integer service
       syscall               # print fibonacci number
       
-      la   $a0, space       # load address of spacer for syscall
+      lasw   $a0, space       # load address of spacer for syscall
+      nop
       ori  $v0, $zero , 4           # specify Print String service
       syscall               # print the spacer string
       
       addi $t0, $t0, 4      # increment address of data to be printed
       addi $t1, $t1, -1     # decrement loop counter
+      nop
+      nop
       bne $t1, $zero , out         # repeat while not finished
+      nop
       
       jr   $ra              # return from subroutine
-      
+      nop
 die:
 # End of subroutine to print the numbers on one line
 ###############################################################
